fa90bd6bb87e27b9d7735ea99e7e24b6cb0f2e33
Berian James <berian.james@gmail.com>
2013-02-21 19:01:52 -0800

    Build parties table around party code rather than party name.

--- src/historical_tables/build_parties.py
+++ src/historical_tables/build_parties.py
@@ -43,11 +43,10 @@ class PartiesTableBuilder(object):
     self.db.execute('DROP TABLE IF EXISTS parties;')
     self.db.execute("""
      CREATE TABLE parties (
-       id INT NOT NULL AUTO_INCREMENT,
        party_code VARCHAR(5),
        party_name VARCHAR(50),
-       party_code_alt VARCHAR(5),
-       PRIMARY KEY(id)
+       party_name_alt VARCHAR(50),
+       PRIMARY KEY(party_code)
        );
      """ )
 
@@ -68,26 +67,28 @@ class PartiesTableBuilder(object):
         split_entry = re.split('\s+',entry)
         party_code = split_entry[1].upper()
         party_name = ' '.join(split_entry[3:])
-        if party_name not in party_dictionary:
-          party_dictionary[party_name] = [party_code]
-        elif party_code not in party_dictionary[party_name]:
-          party_dictionary[party_name].append(party_code)
+        if party_code is None:
+          continue
+        if party_code not in party_dictionary:
+          party_dictionary[party_code] = [party_name]
+        elif party_name not in party_dictionary[party_code]:
+          party_dictionary[party_code].append(party_name)
 
 
   def insert_party_data(self, party_dictionary):
     for party in party_dictionary:
       if len(party_dictionary[party])==1:
-        party_code = party_dictionary[party][0]
-        party_code_alt = 'NULL'
+        party_name = party_dictionary[party][0]
+        party_name_alt = 'NULL'
       else:
-        party_code = party_dictionary[party][0]
-        party_code_alt = party_dictionary[party][1]
+        party_name = party_dictionary[party][0]
+        party_name_alt = party_dictionary[party][1]
       if 'Emergency Committee' in party:
         party = 'Emergency Committee'
       sql = """
-        INSERT INTO parties (party_name, party_code, party_code_alt)
+        INSERT INTO parties (party_name, party_code, party_name_alt)
         VALUES ('%s', '%s', '%s')
-      """ % (party, party_code, party_code_alt)
+      """ % (party_name, party, party_name_alt)
       self.db.execute(sql)
           
 
